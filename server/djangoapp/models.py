5bf62393e502edc5747c642c2342d078c1501a16
nimashakonara <nimasha5889@gmail.com>
2025-09-11 00:42:06 -0400

    Adding temporary changes to Github_4

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,43 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True, null=True)  # Optional field
+
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    TRUCK = 'Truck'
+
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (TRUCK, 'Truck'),
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPES)
+    year = models.IntegerField(
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Optional
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
      path(route='login', view=views.login_user, name='login'),
      path('logout/', views.logout_request, name='logout'), 
      path('registration/', views.registration, name='registration'),
+     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
